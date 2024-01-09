9b4577131e4c63cc2c1687d033cb26e47ff827b8
hardfist <yangjianzju@gmail.com>
2024-01-09 23:13:34 +0800

    chore: test multi rename

--- src/rename.rs
+++ src/rename.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 
 use biome_js_analyze::utils::rename::RenameSymbolExtensions;
-use biome_js_parser::parse;
+use biome_js_parser::{parse, Parse};
 use biome_js_semantic::{semantic_model, SemanticModelOptions};
-use biome_js_syntax::{JsFileSource, JsIdentifierBinding, JsImportNamedClause};
+use biome_js_syntax::{JsFileSource, JsIdentifierBinding, JsImportNamedClause, AnyJsRoot};
 use biome_rowan::{AstNode, BatchMutationExt};
 fn try_new_name(old_name: &str, global_names: &mut HashSet<String>) -> String {
     let mut i = 0;
@@ -18,9 +18,8 @@ fn try_new_name(old_name: &str, global_names: &mut HashSet<String>) -> String {
         }
     }
 }
-pub fn rename_top_level(source: &str, global_names: &mut HashSet<String>) -> String {
-    let r = parse(source, JsFileSource::js_module(), biome_js_parser::JsParserOptions::default());
-    let model = semantic_model(&r.tree(), SemanticModelOptions::default());
+pub fn rename_ast(r: AnyJsRoot) -> AnyJsRoot{
+    let model = semantic_model(&r, SemanticModelOptions::default());
     let bindings: Vec<JsIdentifierBinding> = r
         .syntax()
         .descendants()
@@ -30,16 +29,22 @@ pub fn rename_top_level(source: &str, global_names: &mut HashSet<String>) -> Str
             t.is_none()
         })
         .collect();
-    let mut batch = r.tree().begin();
+    let mut batch = r.begin();
 
     for binding in bindings {
         if let Ok(token) = binding.name_token() {
             let old_name = token.text_trimmed();
-            let new_name = try_new_name(old_name, global_names);
+            let new_name = format!("{old_name}${old_name}");
+            // let new_name = try_new_name(old_name, global_names);
             batch.rename_node_declaration(&model, binding, &new_name);
         }
     }
     let root = batch.commit();
-    dbg!(&root.to_string());
-    root.to_string()
+    return AnyJsRoot::cast(root).unwrap();
+}
+pub fn rename_top_level(source: &str, global_names: &mut HashSet<String>) -> String {
+    let root = parse(source, JsFileSource::js_module(), biome_js_parser::JsParserOptions::default()).tree();
+    let r1 = rename_ast(root);
+    let r2 = rename_ast(r1);
+    dbg!(r2.to_string())
 }
